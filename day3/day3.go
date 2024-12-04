ab7e3e6364d8cf6c2886d33efdc262a7ecb8d764
aljanabim <ma970101@yahoo.se>
2024-12-05 00:04:08 +0100

    Complete day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -9,6 +9,19 @@ import (
 	"github.com/aljanabim/adventofcode2024/utils"
 )
 
+func parseMulti(multi string) int64 {
+	numsStr := strings.Split(multi[4:len(multi)-1], ",")
+	num1, err := strconv.ParseInt(numsStr[0], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	num2, err := strconv.ParseInt(numsStr[1], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return num1 * num2
+}
+
 func solvePart1() int64 {
 	dataRaw, err := os.ReadFile("day3/input")
 	if err != nil {
@@ -20,16 +33,32 @@ func solvePart1() int64 {
 	multis := r.FindAllString(dataStr, -1)
 	var res int64 = 0
 	for _, multi := range multis {
-		numsStr := strings.Split(multi[4:len(multi)-1], ",")
-		num1, err := strconv.ParseInt(numsStr[0], 10, 64)
-		if err != nil {
-			panic(err)
+		res += parseMulti(multi)
+	}
+	return res
+}
+
+func solvePart2() int64 {
+	dataRaw, err := os.ReadFile("day3/input")
+	if err != nil {
+		panic(err)
+	}
+	dataStr := string(dataRaw)
+	// r := regexp.MustCompile(`mul\(\d+,\d+\)`)
+	r := regexp.MustCompile(`do\(\)|don\'t\(\)|mul\(\d+,\d+\)`)
+
+	multis := r.FindAllString(dataStr, -1)
+	var res int64 = 0
+	do := true
+	for _, multi := range multis {
+		if strings.Contains(multi, "'") {
+			do = false
+		} else if strings.Contains(multi, "do(") {
+			do = true
+		} else if strings.Contains(multi, "mul") && do {
+
+			res += parseMulti(multi)
 		}
-		num2, err := strconv.ParseInt(numsStr[1], 10, 64)
-		if err != nil {
-			panic(err)
-		}
-		res += num1 * num2
 	}
 	return res
 }
@@ -37,5 +66,7 @@ func solvePart1() int64 {
 func Solve() {
 	res := solvePart1()
 	utils.PrintSolution(3, 1, res)
+	res = solvePart2()
+	utils.PrintSolution(3, 2, res)
 
 }
