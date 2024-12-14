9ee1a274f67eb553f3f679df16a7c0a659bb3728
aljanabim <ma970101@yahoo.se>
2024-12-14 20:36:03 +0100

    Day 11 part 2 finished

--- day11/day11_test.go
+++ day11/day11_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func aTestPart1(t *testing.T) {
+func TestPart1(t *testing.T) {
 	input := []string{"125", "17"}
 	got := solvePart1(input, 6)
 	want := 22
@@ -28,7 +28,7 @@ func aTestPart1(t *testing.T) {
 	}
 }
 
-func aTestBlink(t *testing.T) {
+func TestBlink(t *testing.T) {
 	got := []string{"0"}
 	for range 4 {
 		got = blink(got)
@@ -40,26 +40,24 @@ func aTestBlink(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
-	input := []string{"0", "20"}
-	solvePart2(input, 4)
-	// input := []string{"125", "17"}
-	// got := solvePart2(input, 6)
-	// want := 22
-	// if got != want {
-	// 	t.Errorf("solvePart2 got %d want %d", got, want)
-	// }
+	input := []string{"125", "17"}
+	got := solvePart2(input, 6)
+	want := 22
+	if got != want {
+		t.Errorf("solvePart2 got %d want %d", got, want)
+	}
 
-	// input = []string{"125", "17"}
-	// got = solvePart2(input, 25)
-	// want = 55312
-	// if got != want {
-	// 	t.Errorf("solvePart2 got %d want %d", got, want)
-	// }
+	input = []string{"125", "17"}
+	got = solvePart2(input, 25)
+	want = 55312
+	if got != want {
+		t.Errorf("solvePart2 got %d want %d", got, want)
+	}
 
-	// input = []string{"0", "1", "1001", "99", "999"} // 1 2024 1 0 9 9 2021976
-	// got = solvePart2(input, 1)
-	// want = 7
-	// if got != want {
-	// 	t.Errorf("solvePart2 got %d want %d", got, want)
-	// }
+	input = []string{"0", "1", "1001", "99", "999"} // 1 2024 1 0 9 9 2021976
+	got = solvePart2(input, 1)
+	want = 7
+	if got != want {
+		t.Errorf("solvePart2 got %d want %d", got, want)
+	}
 }
