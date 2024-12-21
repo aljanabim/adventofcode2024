6be103b16fb20de19615f547aabc8a2f1fa26581
aljanabim <ma970101@yahoo.se>
2024-12-21 02:00:11 +0100

    Solved day 15 part 1

--- day15/day15_test.go
+++ day15/day15_test.go
@@ -1,6 +1,8 @@
 package day15
 
 import (
+	"os"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -16,6 +18,7 @@ func TestComputeGPSSum(t *testing.T) {
 #O.....OO#
 #OO....OO#
 ##########`
+
 	lines := strings.Split(input, "\n")
 	warehouse := readMap(lines)
 	want := 10092
@@ -23,6 +26,100 @@ func TestComputeGPSSum(t *testing.T) {
 	if got != want {
 		t.Errorf("computeGPSSum got %d want %d", got, want)
 
+	}
+}
+
+func TestComputeGPSSum_mini(t *testing.T) {
+	input := `########
+#....OO#
+##.....#
+#.....O#
+#.#O@..#
+#...O..#
+#...O..#
+########`
+
+	lines := strings.Split(input, "\n")
+	warehouse := readMap(lines)
+	want := 2028
+	got := computeGPSSum(&warehouse)
+	if got != want {
+		t.Errorf("computeGPSSum got %d want %d", got, want)
+
+	}
+}
+
+func TestMove_right(t *testing.T) {
+	input1 := `@.OO.O.#`
+	w := readMap([]string{input1})
+
+	got := w.Grid
+	want := [][]Entity{{3, 0, 1, 1, 0, 1, 0, 2}}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Grid after move right: \ngot  %v\nwant %v", want, got)
+	}
+	moves := []Dir{RIGHT}
+	w.Step(moves)
+
+	got = w.Grid
+	want = [][]Entity{{0, 3, 1, 1, 0, 1, 0, 2}}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Grid after move right: \ngot  %v\nwant %v", want, got)
+	}
+
+	moves = []Dir{RIGHT}
+	w.Step(moves)
+	got = w.Grid
+	want = [][]Entity{{0, 0, 3, 1, 1, 1, 0, 2}}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Grid after move right: \ngot  %v\nwant %v", want, got)
+	}
+
+	moves = []Dir{RIGHT}
+	w.Step(moves)
+	got = w.Grid
+	want = [][]Entity{{0, 0, 0, 3, 1, 1, 1, 2}}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Grid after move right: \ngot  %v\nwant %v", want, got)
+	}
+
+	moves = []Dir{RIGHT}
+	w.Step(moves)
+	got = w.Grid
+	want = [][]Entity{{0, 0, 0, 3, 1, 1, 1, 2}}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Grid after move right: \ngot  %v\nwant %v", want, got)
+	}
+
+	moves = []Dir{LEFT, LEFT}
+	w.Step(moves)
+	got = w.Grid
+	want = [][]Entity{{0, 3, 0, 0, 1, 1, 1, 2}}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Grid after move right: \ngot  %v\nwant %v", want, got)
+	}
+}
+
+func TestMove_full(t *testing.T) {
+	rawData, err := os.ReadFile("map_mini.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(rawData), "\n")
+	warehouse := readMap(lines)
+
+	moves, err := readMoves("moves_mini.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	warehouse.Step(moves)
+	got := computeGPSSum(&warehouse)
+	want := 10092
+	if got != want {
+		t.Fatalf("Move full: got %d want %d", got, want)
 	}
 
 }
