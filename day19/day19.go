cd20e98f932529abc7da17e98b438d85d07d7cff
aljanabim <ma970101@yahoo.se>
2024-12-25 18:05:06 +0100

    Solve day 19 part 1

--- day19/day19.go
+++ day19/day19.go
@@ -160,39 +160,59 @@ func getOrder(num int) int {
 	return order
 }
 
-func helper(stripes []int, want, order, maxOrder, val int) int {
+type CacheKey struct {
+	int
+	string
+}
+type CacheVal struct {
+	string
+	bool
+}
+
+func helper(stripes []int, want string, order, maxOrder int, val string, cache map[CacheKey]CacheVal) (string, bool) {
+	// fmt.Println("At order", order, want, "has", val)
 	if order == maxOrder {
-		return 0
+		return "", true
 	}
 	// Only check available stripes if we haven't already got a stripe at this order
-	if getOrder(val) == order+1 {
+	// if getOrder(val) == order+1 {
+	if val, ok := cache[CacheKey{order, val}]; ok {
+		return val.string, val.bool
+	}
+	if len(val) >= order+1 {
 		// fmt.Println("Already has the right order. Skipping order", order)
-		if getDigit(val, order) == getDigit(want, order) {
-			add := helper(stripes, want, order+1, maxOrder, val)
-			if add != -1 {
-				// fmt.Println("==== Returning", val+add, "at order", order)
-				return add
+		// if getDigit(val, order) == getDigit(want, order) {
+		if val[len(val)-order-1] == want[len(want)-order-1] {
+			// fmt.Println("== Existing part of val", val, "for order", order, "works.")
+			add, ok := helper(stripes, want, order+1, maxOrder, val, cache)
+			cache[CacheKey{order, val}] = CacheVal{add, ok}
+			if ok {
+				// fmt.Println("====> Returning", add, "at order", order)
+				return add, true
 			}
 		} else {
-			return -1
+			return "", false
 		}
 
 	} else {
 		for _, stripe := range stripes {
-			// fmt.Println("= Trying stripe", stripe, "at order", order)
-			stripePow := stripe * int(math.Pow10(order))
-			if getDigit(stripePow+val, order) == getDigit(want, order) && getOrder(stripePow) <= maxOrder {
+			// fmt.Println("= Trying stripe", stripe, "at order", order, "want segment", string(want[len(want)-order-1]))
+			// stripePow := stripe * int(math.Pow10(order))
+			stripePow := fmt.Sprintf("%d%s", stripe, val)
+			// if getDigit(stripePow+val, order) == getDigit(want, order) && getOrder(stripePow) <= maxOrder {
+			if stripePow[len(stripePow)-order-1] == want[len(want)-order-1] && len(stripePow) <= maxOrder {
 				// fmt.Println("== Stripe", stripe, "works. Going deeper.")
-				add := helper(stripes, want, order+1, maxOrder, val+stripePow)
-				if add != -1 {
-					// fmt.Println("==== Returning", val+stripePow+add, "at order", order)
-					return stripePow + add
+				add, ok := helper(stripes, want, order+1, maxOrder, stripePow, cache)
+				cache[CacheKey{order, val}] = CacheVal{add, ok}
+				if ok {
+					// fmt.Println("==== Returning", fmt.Sprintf("%s%s", add, stripePow), "at order", order)
+					return fmt.Sprintf("%s%d", add, stripe), true
 				}
 				// fmt.Println("xx Oopps", stripe, "was bad further down the line")
 			}
 		}
 	}
-	return -1
+	return "", false
 }
 
 func parseStripes(stripesStr string) ([]int, map[rune]int) {
@@ -213,26 +233,27 @@ func parseStripes(stripesStr string) ([]int, map[rune]int) {
 	return stripesNum, color2Int
 }
 
-func parseFlags(flags []string, color2Int map[rune]int) []int {
-	flagNums := []int{}
+func parseFlags(flags []string, color2Int map[rune]int) []string {
+	flagNums := []string{}
 	for _, flag := range flags {
-
 		flag = strings.TrimSpace(flag)
-		num := 0
-		for i, c := range flag {
-			num += color2Int[c] * int(math.Pow10(len(flag)-i-1))
+		num := ""
+		for _, c := range flag {
+			// num += color2Int[c] * int(math.Pow10(len(flag)-i-1))
+			num = fmt.Sprintf("%s%d", num, color2Int[c])
 		}
 		flagNums = append(flagNums, num)
 	}
 	return flagNums
 }
 
-func solvePart1(stripes []int, flags []int) int {
+func solvePart1(stripes []int, flags []string) int {
 	count := 0
 	for _, flag := range flags {
-		maxOrder := getOrder(flag)
-		res := helper(stripes, flag, 0, maxOrder, 0)
-		if res != -1 {
+		// fmt.Print("Trying flag ", flag)
+		_, ok := helper(stripes, flag, 0, len(flag), "", map[CacheKey]CacheVal{})
+		if ok {
+			// fmt.Println(" it is okay")
 			count++
 		}
 	}
@@ -244,14 +265,15 @@ func Solve() {
 		panic(err)
 	}
 	stripesStr := lines[0]
+	// flags := lines[7:8]
 	flags := lines[2:]
 	stripes, color2Int := parseStripes(stripesStr)
 	flagNums := parseFlags(flags, color2Int)
-	fmt.Println(color2Int)
-	fmt.Println(stripes)
-	for _, flag := range flagNums {
-		fmt.Println(flag)
-	}
+	// fmt.Println(color2Int)
+	// fmt.Println(stripes, flagNums)
+	// for _, flag := range flagNums {
+	// 	fmt.Println(flag)
+	// }
 
 	res := solvePart1(stripes, flagNums)
 	// too low - 309
--- day19/day19_test.go
+++ day19/day19_test.go
@@ -8,40 +8,39 @@ import (
 
 func TestSolvePart1_simple(t *testing.T) {
 	stripes := []int{0, 22, 3, 12}
-	want := 312
-	maxOrder := getOrder(want)
-	got := helper(stripes, want, 0, maxOrder, 0)
+	want := "312"
+	// maxOrder := getOrder(want)
+	got, _ := helper(stripes, want, 0, len(want), "", map[CacheKey]CacheVal{})
 	if got != want {
-		t.Fatalf("got %d want %d", got, want)
+		t.Fatalf("got %s want %s", got, want)
 	}
 
 }
 
-func _TestSolvePart1_mini(t *testing.T) {
+func TestSolvePart1_mini(t *testing.T) {
 	stripes := []int{0, 1, 21, 3, 4, 325, 13, 43, 31}
 	cases := []struct {
-		arg  int
-		want int
+		arg  string
+		want string
 	}{
-		{31211, 31211},
-		{3441, 3441},
-		{4331, 4331},
-		{113431, 113431},
-		{5325, -1},
-		{325114, 325114},
-		{3141, 3141},
-		{331423, -1},
+		{"31211", "31211"},
+		{"3441", "3441"},
+		{"4331", "4331"},
+		{"113431", "113431"},
+		{"5325", ""},
+		{"325114", "325114"},
+		{"3141", "3141"},
+		{"331423", ""},
 	}
 	for _, c := range cases {
-		got := helper(stripes, c.arg, 0, getOrder(c.arg), 0)
+		got, ok := helper(stripes, c.arg, 0, len(c.arg), "", map[CacheKey]CacheVal{})
 		if got != c.want {
-			t.Errorf("solvePart1(%d)=%d want %d", c.arg, got, c.want)
+			t.Errorf("solvePart1(%s)=%s want %s ok status %t", c.arg, got, c.want, ok)
 		}
 	}
-
 }
 
-func _TestSolvePart1(t *testing.T) {
+func TestSolvePart1(t *testing.T) {
 	lines, err := utils.ReadLines("input_mini.txt")
 	if err != nil {
 		panic(err)
